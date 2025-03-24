21900986c5f767e4baf86c8ecbe161e72ab2edfc
SivaSankar <janaganisiva23@gmail.com>
2025-03-24 22:54:06 +0530

    set the number to stop the series

--- fibinachi.java
+++ fibinachi.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class fibinachi
 {
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the last number of series:");
+        int lastNumber = sc.nextInt();
+
         int firstNum = 0;
         int secondNum = 1;
 
-        while(firstNum <= 100){
+        while(firstNum <= lastNumber){
             int temp = firstNum+secondNum;
             System.out.print(firstNum+" ");
             firstNum = secondNum;
