043927972730e2c9a179d33fe4d43a5a7dc76de3
SivaSankar <janaganisiva23@gmail.com>
2025-04-01 20:16:17 +0530

    Number with out Number

---
+++ PrintNumbersWithOutNumber/PrintNumbersWithOutNumber.java
@@ -0,0 +1,10 @@
+
+public class PrintNumbersWithOutNuners{
+    public static void main(String[] args){
+        String str = "*";
+        String end = "**********";
+        for(int i=str.lenght;i<=end.lenght * end.lenght;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
